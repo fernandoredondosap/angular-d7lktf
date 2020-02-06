84e30af21afec0d8777bf26f17668b526fc4dd2f
fernandoredondosap <fernando.redondo@sap.com>
2020-02-06 16:51:04 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-d7lktf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-d7lktf)
---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { translations, translationChunksConfig } from '@spartacus/assets';
+import {B2cStorefrontModule} from '@spartacus/storefront';
+import {CmsLibModule} from '@spartacus/storefront';
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+
+
+B2cStorefrontModule.withConfig({backend: {
+      occ: {
+        baseUrl: 'https://storefront.c39j2-walkersde1-d4-public.model-t.cc.commerce.ondemand.com/',
+        prefix: '/rest/v2/',
+        legacy: false
+      }
+    },
+    authentication: {
+      client_id: 'mobile_android',
+      client_secret: 'secret'
+    },
+    context: {
+      baseSite: ['electronics']
+    },
+    i18n: {
+        resources: translations,
+        chunks: translationChunksConfig,
+        fallbackLang: 'en'
+      },
+      features: {
+        level: '1.3'
+      }
+    });
+
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HelloComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
