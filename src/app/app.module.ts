107e1662ade8932b1ee260e5a2b7f41c69ac0a81
fernandoredondosap <fernando.redondo@sap.com>
2020-02-06 18:22:57 +0100

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,34 +8,33 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 
-B2cStorefrontModule.withConfig({backend: {
-      occ: {
-        baseUrl: 'https://storefront.c39j2-walkersde1-d4-public.model-t.cc.commerce.ondemand.com/',
-        prefix: '/rest/v2/',
-        legacy: false
-      }
-    },
-    authentication: {
-      client_id: 'mobile_android',
-      client_secret: 'secret'
-    },
-    context: {
-      baseSite: ['electronics']
-    },
-    i18n: {
+
+
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule,
+   B2cStorefrontModule.withConfig({
+      backend: {
+        occ: {
+          baseUrl: 'https://storefront.c39j2-walkersde1-d4-public.model-t.cc.commerce.ondemand.com/',
+          prefix: '/rest/v2/'
+        }
+      },
+      context: {
+        baseSite: ['electronics-spa']
+      },
+      i18n: {
         resources: translations,
         chunks: translationChunksConfig,
         fallbackLang: 'en'
       },
       features: {
-        level: '1.3'
+        level: '1.4',
+        anonymousConsents: true
       }
-    });
-
-
-@NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+    }) ],
   declarations: [ AppComponent, HelloComponent ],
+
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
